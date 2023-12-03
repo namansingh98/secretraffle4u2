c2cd17f70e3e9dd09fd52ce2ab8aa5fd2f1b3423
namansingh98 <naman.rathore@outlook.com>
2023-12-03 16:22:17 +0530

    navfixes

--- app/components/Hero/Navbar/Navbar.js
+++ app/components/Hero/Navbar/Navbar.js
@@ -135,7 +135,7 @@ function Nav() {
             <div className="md:hidden" id="mobile-menu">
               <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 <Link
-                  href="home"
+                  href="/"
                   className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Home
@@ -145,7 +145,7 @@ function Nav() {
                    href="/luckyDraw"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
-                    Lucky Draw Games"
+                    Lucky Draw Games
                 </Link>
 
                 <Link
